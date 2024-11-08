108097dc53099fe969a41d7bb6029f3535c10f96
zhangruijia200610 <zhangruijia200610@163.com>
2024-11-08 10:14:29 +0800

    Add files via upload

---
+++ 1.5.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+#include <iostream>
+using namespace std;
+int main() {
+	float F, C;
+	cout << "请输入华氏温度"；
+		cin >> F;
+	C = (F - 32) * 5 / 9;
+	cout << endl << "对应的摄氏温度：" << setprecision(4) << C;
+	return 0;
+}
---
+++ 2.3.cpp
@@ -0,0 +1,20 @@
+﻿
+#include<iostream>
+using namespace std;
+int main() {
+		double x, y, z;
+		cin >> x >> y >> z;
+		cout << x + y + z;
+	
+		if (x + y > z && x + z > y && y + z > x) {
+
+
+			if （x == y || y == z || z == x）cout << "为等腰三角形" << endl;
+			else cout << "不为等腰三角形" << endl;
+		}
+			else cout << "不能构成三角形" << endl；
+				return 0;
+			}
+				
+
+
