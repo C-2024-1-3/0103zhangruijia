c5c8e7a81891e727ec1ca065ec8a737931c433c8
zhangruijia200610 <zhangruijia200610@163.com>
2024-11-08 09:51:01 +0800

    Delete 2.1.cpp

--- 2.1.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostresam>
-using namespace std;
-int main() {
-	char a;
-	cin >> a;
-	if (a > 96 && a < 123)cout << static_cast<char>(a - 32) << endl;
-	else cout << a + 1 << endl;
-	return 0;
-}
